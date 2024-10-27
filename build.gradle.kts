ab8fba35b3aab8000d025112bd50855d21b6b2d5
Vipul Jha <vipuljha08@gmail.com>
2024-10-27 23:11:55 +0530

    Add dependencies, layouts and drawables
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,12 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.8.3" apply false
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.secrets.gradle.plugin)
+    }
 }
