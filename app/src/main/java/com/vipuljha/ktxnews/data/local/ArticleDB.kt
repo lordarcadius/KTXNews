14a1a5ef269208fd5cb0af5e6b50448162b1888c
Vipul Jha <vipuljha08@gmail.com>
2024-10-31 16:30:01 +0530

    data: Setup room database
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

---
+++ app/src/main/java/com/vipuljha/ktxnews/data/local/ArticleDB.kt
@@ -0,0 +1,34 @@
+package com.vipuljha.ktxnews.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.vipuljha.ktxnews.models.Article
+
+@Database(
+    entities = [Article::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class ArticleDB : RoomDatabase() {
+    abstract fun getArticleDao(): ArticleDao
+
+    companion object {
+        @Volatile
+        private var instance: ArticleDB? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticleDB::class.java,
+                "article_db.db",
+            ).build()
+    }
+}
