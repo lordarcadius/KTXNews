a49f204f9aa74d1c10b634586ee8ebcce9d1a6de
Vipul Jha <vipuljha08@gmail.com>
2024-11-02 15:45:02 +0530

    theme: Add Material 3 color scheme and dynamic theming
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- app/src/main/java/com/vipuljha/ktxnews/NewsApplication.kt
+++ app/src/main/java/com/vipuljha/ktxnews/NewsApplication.kt
@@ -1,6 +1,12 @@
 package com.vipuljha.ktxnews
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 
-class NewsApplication: Application() {
+class NewsApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Apply dynamic color
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
 }
